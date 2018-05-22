cb9e7e38cf6df2f52a4d862bfd57dac06bc52bd3
kcfzyhq <38756332+kcfzyhq@users.noreply.github.com>
2018-05-22 16:55:17 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import urllib
+import urllib.request
+import re
+def download(url, path, id):
+    pagedata = urllib.request.urlopen(url + str(id)).read()
+    file=open( path+ str(id) + ".html","wb") #写入二进制
+    file.write(pagedata)
+    file.close()
+
+url = "https://www.luogu.org/problemnew/show/P"
+for i in range(2724, 4624):
+    print("正在下载第" + str(i) + "题...")
+    download(url, r"C:\Users\LJY\Desktop\dd\\", i)
